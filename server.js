150687a2b066ac5eb0b140ffa27492521f1860e5
Yash Varma <yashvarma@Yashs-MacBook-Pro.local>
2014-12-02 15:35:20 -0800

    update

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use(function(req,res,next){
 });
 
 app.use(function(req, res, next){
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
 
   next();
 });
