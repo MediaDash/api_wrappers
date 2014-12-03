b9b97ae9cc2043acfb029885a824a64b61f817a0
Yash Varma <yashvarma@Yashs-MacBook-Pro.local>
2014-12-02 18:53:55 -0800

    haha

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ var port = process.env.PORT || 9393;    // set our port
 var server = app.listen(3000);
 var http = require('http').Server(app);
 var socket_io = require('socket.io')({
-    "transports": ["xhr-polling", "websockets"],
+    "transports": ["xhr-polling"],
     "polling duration": 10
 });
 
