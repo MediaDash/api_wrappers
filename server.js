6cf15443798b9a8108925facf0a96d0167665076
Yash Varma <yashvarma@Yashs-MacBook-Pro.local>
2014-12-02 16:10:18 -0800

    update

--- server.js
+++ server.js
@@ -70,8 +70,7 @@ var port = process.env.PORT || 9393;    // set our port
 var server = app.listen(3000);
 var http = require('http').Server(app);
 var socket_io = require('socket.io')(http, {
-    "origins": 'http://salty-journey-1875.herokuapp.com',
-    // "transports": ["xhr-polling"],
+    "transports": ['xhr-polling', 'websocket'],
     "polling duration": 10
 });
 
