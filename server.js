bac08947bf72933d7c04148ae6bbf8241ac55f3a
Yash Varma <yashvarma@Yashs-MacBook-Pro.local>
2014-12-03 18:16:45 -0800

    updated sockets

--- server.js
+++ server.js
@@ -104,13 +104,13 @@ app.get('/twitter_stream', function(req, res, next) {
       console.log(data)
       var twitData = (tweetParser().parseTweets({"statuses": [data]}));
       io.emit('tweet', twitData);
-      db.collection('term').insert(twitData, function(err, result){
-        if ( !err ) {
-          return { msg: '' };
-        } else {
-          return { msg: err };
-        }
-      });
+      // db.collection('term').insert(twitData, function(err, result){
+      //   if ( !err ) {
+      //     return { msg: '' };
+      //   } else {
+      //     return { msg: err };
+      //   }
+      // });
     });
   });
 });
